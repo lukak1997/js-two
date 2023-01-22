8b554b41aba4bfa1f110d33fbd95312cb7cd8ca9
lukak1997 <khutsidzeluka@gmail.com>
2023-01-22 11:53:25 -0500

    js-two

---
+++ script.js
@@ -0,0 +1,24 @@
+let populationSpain=47.08;
+populationSpain/=2;
+console.log(populationSpain);
+
+const spain=47.08;
+const quntity=6000000;
+quntity>=6000000;
+console.log(`${quntity} is more then ${spain}`);
+
+
+const country='georgia';
+const continent='europe';
+const language='georgian';
+const citizen=3.72;
+console.log(`i live in ${country}, ${country} is part of ${continent},in oure country speaks in ${language},population is ${3.72}`);
+
+
+const populationGeorgia=3.72;
+const million=5;
+if (3.72>5){
+console.log('population of georgia is more then mollion');
+}else{
+ console.log('population of georgia is less then mollion');
+}    
